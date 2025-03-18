cfceaed889a1d96cc4df2eb74fa107d25d7676ec
glytop <123397208+glytop@users.noreply.github.com>
2025-03-18 16:32:47 +0300

    initial

---
+++ Itransition.Trainee.Web/Itransition.Trainee.Web.Data/WebDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Itransition.Trainee.Web.Data
+{
+    public class WebDbContext : DbContext
+    {
+        
+        
+        
+        public WebDbContext(DbContextOptions<WebDbContext> contextOptions)
+            : base(contextOptions) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
---
+++ Itransition.Trainee.Web/Itransition.Trainee.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Itransition.Trainee.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
