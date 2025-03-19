242fd40c477b1c25b3551721a689f528c5823010
glytop <123397208+glytop@users.noreply.github.com>
2025-03-19 23:35:56 +0300

    add filters and en culture

--- Itransition.Trainee.Web/Itransition.Trainee.Web/wwwroot/js/activity.js
+++ Itransition.Trainee.Web/Itransition.Trainee.Web/wwwroot/js/activity.js
@@ -2,4 +2,17 @@
     $('#selectAll').click(function () {
         $('.user-select').prop('checked', this.checked);
     });
+
+    $("#filterInput").on("keyup", function () {
+        const value = $(this).val().toLowerCase();
+        $("tbody tr").filter(function () {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+        });
+    });
+
+    $("tr:has(.timestamp-popup)").hover(function () {
+        $(this).find(".timestamp-popup").addClass("show");
+    }, function () {
+        $(this).find(".timestamp-popup").removeClass("show");
+    });
 });
